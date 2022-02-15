6a16716cc1116e67ab0255ae758af75b7cf5c68a
IvanY17104 <17104@my.sanctamaria.school.nz>
2022-02-15 19:16:00 +1300

    Changing Laptops

---
+++ components/welcome v2.py
@@ -0,0 +1,17 @@
+import random
+from random import randint
+
+#List of random names
+
+def welcome():
+    names = ["Mark", "Phoebe", "Sally", "Michael", "Denise", "Ellen", "Eric", "Moana", "Lewis", "Lara"]
+    num = randint(0,9)
+    name = (names[num])
+    print("*** Welcome to Dream Pizza***")
+    print("***My name is",name, "***")
+    print("***I will be here to help you order your delicious Dream Pizza***")
+
+def main():
+    welcome()
+
+main()
